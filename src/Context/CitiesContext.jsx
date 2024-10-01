db46529fa5d61569d5e43c5bbf85de7df8f7ce98
Besufkad Mekonnen <besu12mack@gmail.com>
2024-10-01 23:01:21 +0300

    modifying baseurl in citiescontext for deploying on vervel

--- src/Context/CitiesContext.jsx
+++ src/Context/CitiesContext.jsx
@@ -6,7 +6,7 @@ const cityContext = createContext();
 // const BASE_URL = "http://localhost:8000/";
 
 //on vercel deployement
-const BASE_URL = "https://world-wise-cyan-ten.vercel.app/";
+const BASE_URL = "https://world-wise-cyan-ten.vercel.app/api/data";
 
 function CitiesProvider({ children }) {
   const initialStates = {
